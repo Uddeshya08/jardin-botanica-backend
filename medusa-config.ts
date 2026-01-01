cebc8af9b3ff6ca47b0a85a5381f9882bfdf1c03
vishal-kacha <vishaldineshbhaikacha@gmail.com>
2026-01-01 21:33:06 +0530

    added google auth

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,4 +1,8 @@
-import { loadEnv, defineConfig } from "@medusajs/framework/utils";
+import {
+  loadEnv,
+  defineConfig,
+  ContainerRegistrationKeys,
+} from "@medusajs/framework/utils";
 import { Modules } from "@medusajs/framework/utils";
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
@@ -77,6 +81,30 @@ module.exports = defineConfig({
         ],
       },
     },
+
+    {
+      resolve: "@medusajs/medusa/auth",
+      dependencies: [Modules.CACHE, ContainerRegistrationKeys.LOGGER],
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/auth-emailpass",
+
+            id: "emailpass",
+          },
+          {
+            resolve: "@medusajs/medusa/auth-google",
+            id: "google",
+            options: {
+              clientId: process.env.GOOGLE_CLIENT_ID,
+              clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+              callbackUrl: process.env.GOOGLE_CALLBACK_URL,
+            },
+          },
+        ],
+      },
+    },
+
     // {
     //   resolve: "@medusajs/notification",
     //   options: {
